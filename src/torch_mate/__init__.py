77b5c655ac7c9d58680e7d08cad6fdce3a870d80
Douwe den Blanken <douwedb@gmail.com>
2022-12-16 14:33:07 +0100

    Delete old utils file

--- src/torch_mate/__init__.py
+++
@@ -1,7 +0,0 @@
-from fptt.utils.config import get_cfg
-from fptt.utils.contexts import evaluating, training
-from fptt.utils.count_parameters import count_parameters
-from fptt.utils.lstm_cell_to_lstm import lstm_cell_to_lstm
-from fptt.utils.merge_dicts import merge_dicts
-from fptt.utils.run_fptt_wandb_experiment import run_fptt_wandb_experiment
-from fptt.utils.set_seeds import set_seeds
