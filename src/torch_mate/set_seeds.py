a687b5ce1dd99670a7b2cf79c1726341619da873
Douwe den Blanken <douwedb@gmail.com>
2023-01-30 12:12:28 +0100

    Set also CUDA torch seed

--- src/torch_mate/set_seeds.py
+++ src/torch_mate/set_seeds.py
@@ -6,6 +6,7 @@ import torch
 
 def set_seeds(device: torch.device = 'cpu', seed=2147483647):
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
 
