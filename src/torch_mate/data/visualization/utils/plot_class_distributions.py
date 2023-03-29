046e0dad3980cddcf3863a2db5d56ed59eb658c3
Douwe den Blanken <douwedb@gmail.com>
2023-03-29 20:10:20 +0200

    Make plot class distribution work for any kind of dataloader

--- src/torch_mate/data/visualization/utils/plot_class_distributions.py
+++ src/torch_mate/data/visualization/utils/plot_class_distributions.py
@@ -1,42 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
 from torch.utils.data import Dataset
 
+import numpy as np
 
-def plot_class_distribution(dataset: Dataset,
-                            title: str,
-                            shift: int,
-                            width=0.2):
-    labels = np.array([label for _, _, label, _, _ in dataset])
+def plot_class_distribution(dataset: Dataset, title: str, shift: int, width=0.2):
+    labels = np.array([label for _, label in dataset])
 
     unique_label_count = len(np.unique(labels))
-
+    
     # Calculate percentage of each class
-    class_dist = np.array(
-        [np.sum(labels == i) for i in range(unique_label_count)]) / len(labels)
+    class_dist = np.array([np.sum(labels == i) for i in range(unique_label_count)]) / len(labels)
 
-    plt.bar(np.arange(unique_label_count) + shift * width,
-            class_dist,
-            width=width,
-            label=title)
+    plt.bar(np.arange(unique_label_count)+shift*width, class_dist, width=width, label=title)
 
+    return unique_label_count
 
-def plot_class_distributions(train_set: Dataset, val_set: Dataset,
-                             test_set: Dataset):
-    """Plot class distributions of the given dataset splits.
+def plot_class_distributions(train_set: Dataset, val_set: Dataset, test_set: Dataset):
+    label_count = 0
 
-    Args:
-        train_set (Dataset): Training set.
-        val_set (Dataset): Validation set.
-        test_set (Dataset): Test set.
-    """
+    for dataset, title, shift in zip([train_set, val_set, test_set], ['train', 'val', 'test'], [-1, 0, 1]):
+        new_label_count = plot_class_distribution(dataset, title, shift)
 
-    for dataset, title, shift in zip([train_set, val_set, test_set],
-                                     ['train', 'val', 'test'], [0, 1, 2]):
-        plot_class_distribution(dataset, title, shift)
+        if new_label_count != label_count:
+            if  label_count != 0:
+                raise ValueError('Number of classes in train, val and test sets do not match')
+            else:
+                label_count = new_label_count
 
     plt.legend()
     plt.title('Class distribution')
     plt.xlabel('Class index')
     plt.ylabel('Percentage')
+    plt.xticks(np.arange(label_count))
     plt.show()
