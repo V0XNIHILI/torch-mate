ab619f269250f4801d4953b083b9c5776503da7c
Douwe den Blanken <douwedb@gmail.com>
2023-04-28 15:29:01 +0200

    Add arguments to disable method

--- src/torch_mate/disable_torch_debug_apis.py
+++ src/torch_mate/disable_torch_debug_apis.py
@@ -1,7 +1,9 @@
 import torch
 
-def disable_torch_debug_apis():
-    # Taken from: https://betterprogramming.pub/how-to-make-your-pytorch-code-run-faster-93079f3c1f7b
-    torch.autograd.set_detect_anomaly(False)
-    torch.autograd.profiler.profile(False)
-    torch.autograd.profiler.emit_nvtx(False)
+def disable_torch_debug_apis(anomaly=False, profiler=False, nvtx=False):
+    # Disable Debug APIs for final fraining
+     # Taken from: https://betterprogramming.pub/how-to-make-your-pytorch-code-run-faster-93079f3c1f7b
+    torch.autograd.set_detect_anomaly(anomaly)
+    torch.autograd.profiler.profile(profiler)
+    torch.autograd.profiler.emit_nvtx(nvtx)
+
