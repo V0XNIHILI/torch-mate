2c266236c7ec114da21186a284adaf5fc71e15f0
Douwe den Blanken <douwedb@gmail.com>
2023-04-28 15:30:03 +0200

    Add extension for getting multiple devices to get_device

--- src/torch_mate/get_device.py
+++ src/torch_mate/get_device.py
@@ -1,8 +1,18 @@
 import torch
 
+from typing import List, Union
 
-def get_device(cuda_index: int = 0):
-    device = torch.device(
-        f"cuda:{cuda_index}" if torch.cuda.is_available() else "cpu")
+import os
+
+
+def get_device(cuda_index: Union[List[int], int] = 0) -> torch.device:
+    if isinstance(cuda_index, list):
+        os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"]=','.join([str(d) for d in cuda_index])
+
+        device = torch.device('cuda')
+    else:
+        device = torch.device(
+            f"cuda:{cuda_index}" if torch.cuda.is_available() else "cpu")
 
     return device
