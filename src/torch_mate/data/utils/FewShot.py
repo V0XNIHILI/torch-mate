9112cc1f078fad049857f6260183c191fa7aeac2
Douwe den Blanken <douwedb@gmail.com>
2023-08-12 11:25:54 +0300

    Add support for cumulative query set

--- src/torch_mate/data/utils/FewShot.py
+++ src/torch_mate/data/utils/FewShot.py
@@ -28,6 +28,7 @@ class FewShot(IterableDataset):
                  k_shot: int,
                  query_shots: int,
                  incremental: bool = False,
+                 cumulative: bool = False,
                  always_include_classes: Optional[List[int]] = None,
                  transform: Optional[Callable] = None,
                  per_class_transform: Optional[Callable] = None):
@@ -39,11 +40,27 @@ class FewShot(IterableDataset):
             n_way (int): Number of classes per batch.
             k_shot (int): Number of samples per class in the support set.
             incremental (bool, optional): Whether to incrementally sample classes. Defaults to False.
+            cumulative (bool, optional): Whether to increase the query set size with each iteration. This flag will only work when incremental is set to True. Defaults to False.
             query_shots (int): Number of samples per class in the query set.
-            always_include_classes (Optional[List[int]], optional): List of classes to always include in the batch. Defaults to None.
-            transform (Optional[Callable], optional): Transform applied to every data sample. Will be reapplied every time a batch is servedd. Defaults to None.
+            always_include_classes (Optional[List[int]], optional): List of classes to always include in the batch, both in the support and query set. Defaults to None.
+            transform (Optional[Callable], optional): Transform applied to every data sample. Will be reapplied every time a batch is served. Defaults to None.
             per_class_transform (Optional[Callable], optional): Transform applied to every data sample. Will only be applied once per class. Defaults to None.
         """
+
+        if cumulative and not incremental:
+            raise ValueError(
+                "cumulative can only be set to True when incremental is True."
+            )
+
+        if always_include_classes is not None:
+            if len(always_include_classes) > n_way:
+                raise ValueError("always_include_classes cannot have more elements than n_way.")
+
+            if len(set(always_include_classes)) != len(always_include_classes):
+                raise ValueError("always_include_classes cannot have duplicate elements.")
+
+            if cumulative:
+                raise ValueError("always_include_classes cannot be used when cumulative is True.")
 
         self.dataset = dataset
         self.indices_per_class = get_indices_per_class(self.dataset)
@@ -57,11 +74,12 @@ class FewShot(IterableDataset):
         self.transform = transform
         self.per_class_transform = per_class_transform
 
+        total_classes = len(self.indices_per_class)
+
         if incremental:
-            self.class_sampler = BatchSampler(SequentialSampler(range(len(self.indices_per_class))), batch_size=self.n_way, drop_last=True)
+            self.class_sampler = BatchSampler(SequentialSampler(range(total_classes)), batch_size=self.n_way, drop_last=True)
         else:
-            self.class_sampler = iter(
-                InfiniteClassSampler(len(self.indices_per_class), self.n_way))
+            self.class_sampler = InfiniteClassSampler(total_classes, self.n_way)
 
     def __iter__(self):
         """Get a batch of samples for a k-shot n-way task.
@@ -70,35 +88,47 @@ class FewShot(IterableDataset):
             tuple[tuple[Tensor, Tensor], tuple[Tensor, Tensor]]: The support and query sets in the form of ((X_support, X_query), (y_support, y_query))
         """
 
-        for class_indices in (self.class_sampler):
+        cumulative_classes = []
+
+        for new_class_indices in (self.class_sampler):
             X_train_samples = []
             X_test_samples = []
 
             y_train_samples = []
             y_test_samples = []
 
+            class_indices = new_class_indices + cumulative_classes
+
+            if cumulative:
+                cumulative_classes.extend(new_class_indices)
+
             if self.always_include_classes is not None:
+                # This line also makes sure that the always include classes are always
+                # in the same position in the batch
                 class_indices = list(
                     set(class_indices) - set(self.always_include_classes)
                 )[:len(class_indices) - len(self.always_include_classes
                                             )] + self.always_include_classes
 
             for i, cls in enumerate(class_indices):
-                y_train_samples.extend([i] * self.k_shot)
-                y_test_samples.extend([i] * self.query_shots)
-
                 within_class_indices = np.random.choice(
                     self.indices_per_class[cls],
                     self.k_shot + self.query_shots,
                     replace=False)
 
                 class_samples = torch.stack(
-                    [self.dataset[i][0] for i in within_class_indices])
+                    [self.dataset[j][0] for j in within_class_indices])
 
                 if self.per_class_transform is not None:
                     class_samples = self.per_class_transform(class_samples)
 
-                X_train_samples.extend(class_samples[:self.k_shot])
+                # Only in the case of cumulative we need to make sure that we only
+                # include the new classes in the support set and not the previous classes
+                if i < self.n_way:
+                    y_train_samples.extend([i] * self.k_shot)
+                    X_train_samples.extend(class_samples[:self.k_shot])
+
+                y_test_samples.extend([i] * self.query_shots)
                 X_test_samples.extend(class_samples[self.k_shot:])
 
             X_samples = torch.stack(X_train_samples + X_test_samples)
@@ -106,8 +136,9 @@ class FewShot(IterableDataset):
             if self.transform is not None:
                 X_samples = self.transform(X_samples)
 
-            X_train_samples = X_samples[:len(class_indices) * self.k_shot]
-            X_test_samples = X_samples[len(class_indices) * self.k_shot:]
+            num_train_samples = len(class_indices) * self.k_shot
+            X_train_samples = X_samples[:num_train_samples]
+            X_test_samples = X_samples[num_train_samples:]
 
             out = ((X_train_samples,
                     X_test_samples), (torch.tensor(y_train_samples),
