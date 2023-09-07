f4f8208eeab130791d205b8c41ce0b9d7052344c
Douwe den Blanken <douwedb@gmail.com>
2023-09-07 16:15:01 +0200

    Make sure import of DataSampler works

--- src/torch_mate/data/samplers/__init__.py
+++ src/torch_mate/data/samplers/__init__.py
@@ -2,3 +2,5 @@ from torch_mate.data.samplers.ImbalancedClassSampler import \
     ImbalancedClassSampler
 from torch_mate.data.samplers.InfiniteClassSampler import \
     InfiniteClassSampler
+
+from torch_mate.data.samplers.DataSampler import DataSampler
