0bdeb407dfed456f73091b540f6a86fad4e74ffb
Douwe den Blanken <douwedb@gmail.com>
2023-09-25 11:48:32 +0200

    Add transform and target transform to Hasy

--- src/torch_mate/data/datasets/HASYv2.py
+++ src/torch_mate/data/datasets/HASYv2.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 from hasy.hasy_tools import load_data
 
 class HASYv2(Dataset):
-    def __init__(self, transform: Union[Callable, None] = None) -> None:
+    def __init__(self, transform: Union[Callable, None] = None, target_transform: Optional[Callable] = None) -> None:
         """HASYv2 dataset, see https://arxiv.org/pdf/1701.08380 for details.
         """
 
@@ -14,8 +14,20 @@ class HASYv2(Dataset):
         self.X = data['x']
         self.y = data['y']
 
+        self.transform = transform
+        self.target_transform = target_transform
+
     def __getitem__(self, index: int):
-        return self.X[index], self.y[index]
+        X = self.X[index]
+        y = self.y[index][0]
+
+        if self.transform:
+            X = self.transform(X)
+
+        if self.target_transform:
+            y = self.target_transform(y)
+
+        return X, y
 
     def __len__(self):
         return len(self.y)
