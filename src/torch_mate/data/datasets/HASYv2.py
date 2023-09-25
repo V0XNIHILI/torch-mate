f03156929aafa24bec4645e944377a375b795a2f
Douwe den Blanken <douwedb@gmail.com>
2023-09-25 11:45:44 +0200

    Add docstring to Hasy

--- src/torch_mate/data/datasets/HASYv2.py
+++ src/torch_mate/data/datasets/HASYv2.py
@@ -1,9 +1,14 @@
+from typing import Union, Optional, Callable
+
 from torch.utils.data import Dataset
 
 from hasy.hasy_tools import load_data
 
 class HASYv2(Dataset):
-    def __init__(self):
+    def __init__(self, transform: Union[Callable, None] = None) -> None:
+        """HASYv2 dataset, see https://arxiv.org/pdf/1701.08380 for details.
+        """
+
         data = load_data(mode="complete")
 
         self.X = data['x']
