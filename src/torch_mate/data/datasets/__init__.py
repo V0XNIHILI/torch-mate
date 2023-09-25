f04888fb72d8e9add9ac39fd935a41b296c497d3
Douwe den Blanken <douwedb@gmail.com>
2023-09-25 11:06:51 +0200

    Rename SpeechCommands_35C to all caps filename

--- src/torch_mate/data/datasets/__init__.py
+++ src/torch_mate/data/datasets/__init__.py
@@ -1,3 +1,3 @@
 from torch_mate.data.datasets.HASYv2 import HASYv2
 from torch_mate.data.datasets.MSWC import MSWC
-from torch_mate.data.datasets.SpeechCommands_35C import SpeechCommands_35C
+from torch_mate.data.datasets.SPEECHCOMMANDS_35C import SPEECHCOMMANDS_35C
