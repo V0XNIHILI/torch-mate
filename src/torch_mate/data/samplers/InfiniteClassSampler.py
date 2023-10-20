20197b7918a07b9adc84baa93ba6118fbe3b00da
Douwe den Blanken <douwedb@gmail.com>
2023-10-20 14:41:41 +0200

    Make sure InfiniteClassSampler samples from a given list instead of from a range(input_len)

--- src/torch_mate/data/samplers/InfiniteClassSampler.py
+++ src/torch_mate/data/samplers/InfiniteClassSampler.py
@@ -6,19 +6,17 @@ from torch.utils.data import Sampler
 
 class InfiniteClassSampler(Sampler[List[int]]):
 
-    def __init__(self, total_classes: int, n_way: int):
+    def __init__(self, class_indices: List[int], n_way: int):
         """Creates a sampler that samples classes infinitely (or effectively
         samples sys.maxsize classes)
 
         Args:
-            total_classes (int): Total classes the dataset has.
+            class_indices (List[int]): All classes the dataset has.
             n_way (int): Number of classes per batch.
         """
 
-        self.total_classes = total_classes
+        self.classes_list = class_indices
         self.n_way = n_way
-
-        self.classes_list = list(range(total_classes))
 
     def __iter__(self):
         while True:
