abf532be60b039e7973887e66402d6664ba870e6
Douwe den Blanken <douwedb@gmail.com>
2023-10-24 14:48:04 +0200

    Add docstring to get_device

--- src/torch_mate/utils/get_device.py
+++ src/torch_mate/utils/get_device.py
@@ -6,13 +6,24 @@ import os
 
 
 def get_device(cuda_index: Union[List[int], int] = 0) -> torch.device:
-    if isinstance(cuda_index, list):
-        os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"]=','.join([str(d) for d in cuda_index])
+    """Get one or more CUDA devices. In case CUDA is not available, the CPU is used.
 
-        device = torch.device('cuda')
+    Args:
+        cuda_index (Union[List[int], int], optional): Which CUDA device index/indices to use. Defaults to 0.
+
+    Returns:
+        torch.device: The CUDA device(s) to use.
+    """
+
+    if torch.cuda.is_available():
+        if isinstance(cuda_index, list):
+            os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+            os.environ["CUDA_VISIBLE_DEVICES"]=','.join([str(d) for d in cuda_index])
+
+            device = 'cuda'
+        else:
+            device = f"cuda:{cuda_index}"
     else:
-        device = torch.device(
-            f"cuda:{cuda_index}" if torch.cuda.is_available() else "cpu")
+        device = "cpu"
 
-    return device
+    return torch.device(device)
