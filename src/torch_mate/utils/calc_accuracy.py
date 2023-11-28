6642f7eb9a4fbccdc2d6d119630416fe12d38389
Douwe den Blanken <douwedb@gmail.com>
2023-11-28 14:45:28 +0100

    Add support for top-k accuracy

--- src/torch_mate/utils/calc_accuracy.py
+++ src/torch_mate/utils/calc_accuracy.py
@@ -1,7 +1,19 @@
 import torch
 
 
-def calc_accuracy(predictions: torch.Tensor, targets: torch.Tensor):
-    predictions = predictions.argmax(dim=1).view(targets.shape)
+def calc_accuracy(predictions: torch.Tensor, targets: torch.Tensor, k: int = 1) -> torch.Tensor:
+    """Calculate the accuracy of the predictions.
 
-    return (predictions == targets).sum().float() / targets.size(0)
+    Args:
+        predictions (torch.Tensor): Predictions. Shape: (batch_size, num_classes)
+        targets (torch.Tensor): Targets. Shape: (batch_size)
+        k (int, optional): Top-k accuracy. Defaults to 1.
+
+    Returns:
+        torch.Tensor: Accuracy.
+    """
+
+    _, top_k_indices = predictions.topk(k, dim=1)
+    repeated_targets = targets.repeat(k, 1).t()
+
+    return (top_k_indices == repeated_targets).sum().float() / targets.size(0)
