f257643a9836219bd2ee3e324d8cd6cc44fe2728
Douwe den Blanken <douwedb@gmail.com>
2023-11-28 14:44:15 +0100

    Add notebook for demo of creating a WandB sweep

--- src/torch_mate/utils/__init__.py
+++ src/torch_mate/utils/__init__.py
@@ -1,6 +1,5 @@
 from .calc_accuracy import calc_accuracy
 from .iterate_to_device import iterate_to_device
-from .merge_dicts import merge_dicts
 from .tuples_to_device import nested_tuple_to_device
 from .count_parameters import count_parameters
 from .get_device import get_device
