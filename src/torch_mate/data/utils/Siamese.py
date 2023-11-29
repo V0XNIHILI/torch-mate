1f726c25fc7c6205c304e59920c81f557b60ce65
Douwe den Blanken <douwedb@gmail.com>
2023-11-29 16:42:26 +0100

    Convert Siamese dataset into an iterable dataset

--- src/torch_mate/data/utils/Siamese.py
+++ src/torch_mate/data/utils/Siamese.py
@@ -1,34 +1,37 @@
-from torch.utils.data import Dataset
+from torch.utils.data import IterableDataset, Dataset
 
 import random
 
+from torch_mate.data.utils.get_indices_per_class import get_indices_per_class
 
-class Siamese(Dataset):
-    def __init__(self, dataset: Dataset, length: int):
+
+class Siamese(IterableDataset):
+    def __init__(self, dataset: Dataset):
+        super(Siamese).__init__()
+
         self.dataset = dataset
 
-        self.indices_per_class = get_indices_per_class(self.dataset, self.support_query_split, samples_per_class)
+        self.indices_per_class = get_indices_per_class(self.dataset)
 
         self.classes = list(self.indices_per_class.keys())
 
-        self.length = length
+    def generate(self):
+        while True:
+            same_label = random.random() < 0.5
 
-    def __len__(self):
-        return self.length
+            label1 = random.choice(self.classes)
 
-    def __getitem__(self, index):
-        same_label = random.random() < 0.5
+            if same_label:
+                indices = random.sample(self.indices_per_class[label1], 2)
+            else:
+                classes_without_label1 = [label for label in self.classes if label != label1]
 
-        label1 = random.choice(self.classes)
+                # Make sure that the two labels are different
+                label2 = random.choice(classes_without_label1)
 
-        if same_label:
-            indices = random.sample(self.indices_per_class[label1], 2)
-        else:
-            classes_without_label1 = [label for label in self.classes if label != label1]
+                indices = [random.choice(self.indices_per_class[label1]), random.choice(self.indices_per_class[label2])]
 
-            # Make sure that the two labels are different
-            label2 = random.choice(classes_without_label1)
+            yield (self.dataset[indices[0]][0], self.dataset[indices[1]][0]), same_label
 
-            indices = [random.choice(self.indices_per_class[label1]), random.choice(self.indices_per_class[label2])]
-
-        return (self.dataset[indices[0]][0], self.dataset[indices[1]][0]), same_label
+    def __iter__(self):
+        return iter(self.generate())
