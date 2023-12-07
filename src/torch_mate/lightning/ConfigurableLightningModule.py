e2b4dc31d87ab7f9aa42db334eb2aa65288474af
Douwe den Blanken <douwedb@gmail.com>
2023-12-07 17:26:13 +0100

    Make direct use of self.hparams  and add docstring

--- src/torch_mate/lightning/ConfigurableLightningModule.py
+++ src/torch_mate/lightning/ConfigurableLightningModule.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Dict
 
 import torch.nn as nn
 import torch.optim as optim
@@ -8,24 +8,44 @@ import torchvision
 import lightning as L
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
 
-from dotmap import DotMap
-
 from torch_mate.utils import get_class, calc_accuracy
 
 
 class ConfigurableLightningModule(L.LightningModule):
-    def __init__(self, cfg: DotMap, model: Optional[nn.Module] = None):
+    def __init__(self, cfg: Dict):
+        """Lightweight wrapper around PyTorch LightningModule that adds support for a configuration dictionary.
+
+        Adds the following three attributes:
+        
+        - self.model: the created model
+        - self.criterion: the created criterion
+        - self.generic_step(self, batch, batch_idx, phase): a generic step function that is shared across all steps (train, val, test, predict)
+
+        Based on these, the following methods are automatically implemented:
+
+        - self.forward(self, x): calls self.model(x)
+        - self.training_step(self, batch, batch_idx): calls self.generic_step(batch, batch_idx, "train")
+        - self.validation_step(self, batch, batch_idx): calls self.generic_step(batch, batch_idx, "val")
+        - self.test_step(self, batch, batch_idx): calls self.generic_step(batch, batch_idx, "test")
+        - self.predict_step(self, batch, batch_idx): calls self.generic_step(batch, batch_idx, "predict")
+
+        As a baseline, self.generic_step support (top-k) classification and regression tasks.
+
+        Args:
+            cfg (Dict): configuration dictionary
+        """
+
         super().__init__()
 
-        self.cfg = cfg
-        self.save_hyperparameters(cfg.toDict())
+        self.save_hyperparameters(cfg)
 
-        self.model = model if model else get_class(torchvision.models, cfg.model.name)(**cfg.model.cfg.toDict())
-        self.loss = get_class(nn, cfg.criterion.name)()
+        self.model = get_class(torchvision.models, self.hparams.model["name"])(**self.hparams.model["cfg"])
+        self.criterion = get_class(nn, self.hparams.criterion["name"])()
 
     def configure_optimizers(self) -> OptimizerLRScheduler:
-        optimizer = get_class(optim, self.cfg.optimizer.name)(self.model.parameters(),  **self.cfg.optimizer.cfg.toDict())
-        scheduler = get_class(optim.lr_scheduler, self.cfg.lr_scheduler.name)(optimizer, **self.cfg.lr_scheduler.cfg.toDict()) if self.cfg.lr_scheduler else None
+        # TODO: support multiple optimizers and schedulers
+        optimizer = get_class(optim, self.hparams.optimizer["name"])(self.model.parameters(),  **self.hparams.optimizer["cfg"])
+        scheduler = get_class(optim.lr_scheduler, self.hparams.lr_scheduler["name"])(optimizer, **self.hparams.lr_scheduler["cfg"]) if "lr_scheduler" in self.hparams else None
 
         if scheduler is not None:
             return [optimizer], [scheduler]
@@ -40,13 +60,14 @@ class ConfigurableLightningModule(L.LightningModule):
 
         output = self(x)
 
-        loss = self.loss(output, y)
+        loss = self.criterion(*output if isinstance(output, tuple) else output, y)
 
         prog_bar = phase == 'val'
 
         self.log(f"{phase}/loss", loss, prog_bar=prog_bar)
 
-        if self.cfg.task.classification and self.cfg.task.classification == True:
+        # TODO: add top-k support
+        if self.hparams.task.get("classification", False) == True:
             self.log(f"{phase}/accuracy", calc_accuracy(output, y), prog_bar=prog_bar)
 
         return loss
