2bde7b30f7ccf2d837b6669f739f9de6ce776de2
Douwe den Blanken <douwedb@gmail.com>
2023-12-09 20:45:10 +0100

    Make sure ConfigurableLightningDataModule only uses Dict

--- src/torch_mate/lightning/ConfigurableLightningDataModule.py
+++ src/torch_mate/lightning/ConfigurableLightningDataModule.py
@@ -4,9 +4,30 @@ import lightning as L
 
 from torch.utils.data import DataLoader
 
-from torch_mate.lightning.utils import build_data_loader_kwargs, create_state_transforms, build_transform
+from torch_mate.lightning.utils import build_data_loader_kwargs, create_state_transforms, build_transform, BuiltTransform, StateTransform
+
+STAGES = ['train', 'val', 'test', 'predict']
+MOMENTS = ["pre", "post"]
 
 class ConfigurableLightningDataModule(L.LightningDataModule):
+    train_dataloader_kwargs: Dict
+    val_dataloader_kwargs: Dict
+    test_dataloader_kwargs: Dict
+    predict_dataloader_kwargs: Dict
+
+    train_transforms: StateTransform
+    val_transforms: StateTransform
+    test_transforms: StateTransform
+    predict_transforms: StateTransform
+
+    train_target_transforms: BuiltTransform
+    val_target_transforms: BuiltTransform
+    test_target_transforms: BuiltTransform
+    predict_target_transforms: BuiltTransform
+
+    pre_transfer_batch_transform: BuiltTransform
+    post_transfer_batch_transform: BuiltTransform
+
     def __init__(self, cfg: Dict):
         """Lightweight wrapper around PyTorch Lightning LightningDataModule that adds support for configuration via a dictionary.
 
@@ -28,33 +49,40 @@ class ConfigurableLightningDataModule(L.LightningDataModule):
 
         super().__init__()
 
-        self.cfg = cfg
+        self.save_hyperparameters(cfg)
 
-        self.save_hyperparameters(cfg.toDict())
+        common_pre_transforms, common_post_transforms = [build_transform(self.hparams.task.get("transforms", {}).get(m, [])) for m in MOMENTS]
 
-        self.train_dataloader_kwargs = build_data_loader_kwargs(cfg.task.train, cfg.data_loaders, 'train')
-        self.val_dataloader_kwargs = build_data_loader_kwargs(cfg.task.val, cfg.data_loaders, 'val')
-        self.test_dataloader_kwargs = build_data_loader_kwargs(cfg.task.test, cfg.data_loaders, 'test')
+        data_loaders_cfg = self.hparams.get("data_loaders", {})
+        
         # TODO: add support for predict dataloader kwargs
+        for stage in STAGES:
+            task_stage_cfg = self.hparams.task.get(stage, {})
 
-        common_pre_transforms = build_transform(cfg.task.transforms.pre) if cfg.task.transforms and cfg.task.transforms.pre else None
-        common_post_transforms = build_transform(cfg.task.transforms.post) if cfg.task.transforms and cfg.task.transforms.post else None
+            setattr(self, f"{stage}_dataloader_kwargs", build_data_loader_kwargs(
+                task_stage_cfg,
+                data_loaders_cfg,
+                stage)
+            )
 
-        self.train_transforms = create_state_transforms(cfg.task.train, common_pre_transforms, common_post_transforms)
-        self.val_transforms = create_state_transforms(cfg.task.val, common_pre_transforms, common_post_transforms)
-        self.test_transforms = create_state_transforms(cfg.task.test, common_pre_transforms, common_post_transforms)
-        # TODO: add support for predict transforms
+            setattr(self, f"{stage}_transforms", create_state_transforms(
+                task_stage_cfg,
+                common_pre_transforms, 
+                common_post_transforms
+            ))
 
-        self.train_target_transforms = build_transform(cfg.task.train.target_transforms) if cfg.task.train.target_transforms else None
-        self.val_target_transforms = build_transform(cfg.task.val.target_transforms) if cfg.task.val.target_transforms else None
-        self.test_target_transforms = build_transform(cfg.task.test.target_transforms) if cfg.task.test.target_transforms else None
+            setattr(self, f"{stage}_target_transforms", build_transform(
+                task_stage_cfg.get("target_transforms", [])
+            ))
 
-        if cfg.task.transforms and cfg.task.transforms.batch:
-            if cfg.task.transforms.batch.pre:
-                self.pre_transfer_batch_transform = build_transform(cfg.task.transforms.batch.pre)
-            
-            if cfg.task.transforms.batch.post:
-                self.post_transfer_batch_transform = build_transform(cfg.task.transforms.batch.post)
+        if self.hparams.task.get("transforms", {}).get("batch", None):
+            for m in MOMENTS:
+                setattr(self, f"{m}_transfer_batch_transform", build_transform(
+                    self.hparams.task["transforms"]["batch"].get(m, [])
+                ))
+        else:
+            self.pre_transfer_batch_transform = None
+            self.post_transfer_batch_transform = None
 
     def get_dataset(self, phase: str):
         raise NotImplementedError
@@ -75,14 +103,14 @@ class ConfigurableLightningDataModule(L.LightningDataModule):
         return batch
     
     def on_before_batch_transfer(self, batch, dataloader_idx: int):
-        if hasattr(self, "post_transfer_batch_transform"):
+        if self.post_transfer_batch_transform is not None:
             batch = self.reshape_batch_during_transfer(batch, dataloader_idx, "before")
-            batch = self.post_transfer_batch_transform()
+            batch = self.post_transfer_batch_transform(batch)
         
         return batch
     
     def on_after_batch_transfer(self, batch, dataloader_idx: int):
-        if hasattr(self, "pre_transfer_batch_transform"):
+        if self.pre_transfer_batch_transform is not None:
             batch = self.reshape_batch_during_transfer(batch, dataloader_idx, "after")
             batch = self.pre_transfer_batch_transform(batch)
         
