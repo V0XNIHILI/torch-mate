163f2b0c1fb354839d769e9cb8c842f9c421d312
Douwe den Blanken <douwedb@gmail.com>
2023-12-09 20:52:51 +0100

    Make sure self_supervised forward functions actually return something

--- src/torch_mate/lightning/lm/self_supervised.py
+++ src/torch_mate/lightning/lm/self_supervised.py
@@ -1,17 +1,17 @@
-from torch_mate.lightning.lm import SupervisedLearner
+import torch_mate.lightning.lm as lm
 from torch_mate.lightning import ConfigurableLightningModule
 
 from torch_mate.lightning.lm.functional import siamese as LFSiamese
 from torch_mate.lightning.lm.functional import triplet as LFTriplet
 
-class SiameseNetwork(SupervisedLearner):
+class SiameseNetwork(lm.SupervisedLearner):
     def forward(self, x):
-        LFSiamese.forward(self, x)
+        return LFSiamese.forward(self, x)
 
 
 class TripletNetwork(ConfigurableLightningModule):
     def forward(self, x):
-        LFTriplet.forward(self, x)
+        return LFTriplet.forward(self, x)
 
     def generic_step(self, batch, batch_idx, phase: str):
         return LFTriplet.generic_step(self, batch, batch_idx, phase)
