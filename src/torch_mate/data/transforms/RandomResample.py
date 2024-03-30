88a5268d4d040eba50497ce853e56a8654596122
Douwe den Blanken <douwedb@gmail.com>
2024-03-30 16:17:39 +0100

    Make random resample transform work

--- src/torch_mate/data/transforms/RandomResample.py
+++ src/torch_mate/data/transforms/RandomResample.py
@@ -5,11 +5,25 @@ import random
 import torch
 from torchaudio.functional import resample
 
+
 class RandomResample:
-    def __init__(self, sample_rate: int, resample_range: Tuple[int, int]):
-        self.sample_rate = sample_rate
+    def __init__(self, orig_freq: int, resample_range: Tuple[int, int]):
+        self.orig_freq = orig_freq
         self.resample_range = resample_range
 
     def __call__(self, waveform: torch.Tensor) -> torch.Tensor:
-        new_sample_rate = random.randint(*self.resample_range)
-        return resample(waveform, self.sample_rate, new_sample_rate)
+        new_sample_freq = random.randint(*self.resample_range)
+        resampled_waveform = resample(waveform, self.orig_freq, new_sample_freq)
+
+        # Pad with zeros at the start if the resampled waveform is shorter,
+        # otherwise, cut the waveform
+        
+        if resampled_waveform.size(1) < waveform.size(1):
+            resampled_waveform = torch.cat(
+                (torch.zeros(waveform.size(0), waveform.size(1) - resampled_waveform.size(1), device=waveform.device), resampled_waveform),
+                dim=1
+            )
+        else:
+            resampled_waveform = resampled_waveform[:, :waveform.size(1)]
+
+        return resampled_waveform
