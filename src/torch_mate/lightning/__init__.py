d19e3efb0e2c94cc8f0bd896aa1be7aba24950b5
Douwe den Blanken <douwedb@gmail.com>
2024-04-11 00:59:14 +0200

    Use __all__ in lightning init

--- src/torch_mate/lightning/__init__.py
+++ src/torch_mate/lightning/__init__.py
@@ -1,3 +1,13 @@
 from .ConfigurableLightningModule import ConfigurableLightningModule
 from .ConfigurableLightningDataModule import ConfigurableLightningDataModule
 from .configuration import configure_trainer, configure_data, configure_model, configure_model_data, configure_stack
+
+__all__ = [
+    "ConfigurableLightningModule",
+    "ConfigurableLightningDataModule",
+    "configure_trainer",
+    "configure_data",
+    "configure_model",
+    "configure_model_data",
+    "configure_stack"
+]
