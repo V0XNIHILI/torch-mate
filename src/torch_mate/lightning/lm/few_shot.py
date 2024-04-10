8590ab6104750628b80065df1cd88a6534cc4aef
Douwe den Blanken <douwedb@gmail.com>
2024-04-10 11:56:25 +0200

    Remove MetaBaseline code

--- src/torch_mate/lightning/lm/few_shot.py
+++ src/torch_mate/lightning/lm/few_shot.py
@@ -7,32 +7,32 @@ from torch_mate.lightning import ConfigurableLightningModule
 
 import torch_mate.lightning.lm.functional.prototypical as LFPrototypical
 import torch_mate.lightning.lm.functional.supervised as LFSupervised
+# import torch_mate.lightning.lm.functional.maml as LFMAML
 
+# from torch_mate.lightning.utils.MAML import MAML as PTMAML
 
 class PrototypicalNetwork(ConfigurableLightningModule):
     def generic_step(self, batch, batch_idx, phase: str):
         return LFPrototypical.generic_step(self, batch, batch_idx, phase)
 
+# class MAML(ConfigurableLightningModule):
+#     def __init__(self, cfg: Dict):
+#         """Implements Model-Agnostic Meta-Learning (https://arxiv.org/abs/1703.03400)
 
-class MetaBaseline(PrototypicalNetwork):
-    def __init__(self, cfg: Dict):
-        """Implements Meta-Baseline (https://arxiv.org/abs/2003.04390)
+#         Args:
+#             cfg (Dict): configuration dictionary
+#         """
+            
+#         super().__init__(cfg)
+    
+#         self.maml = PTMAML(self.model, self.hparams.few_shot["cfg"]["inner_loop_lr"], self.hparams.few_shot["cfg"].get("first_order", False))
 
-        Args:
-            cfg (Dict): configuration dictionary
-        """
-
-        super().__init__(cfg)
-
-        self.linear = nn.Linear(self.hparams.pre_training["embedding_size"], self.hparams.task["train"]["num_classes"])
-
-    def generic_step(self, batch, batch_idx, phase: str):
-        if phase == 'train':
-            x, y = batch
-
-            output = self.linear(self.model(x))
-            loss = LFSupervised.compute_loss(self.criterion, output, y)
-
-            return output, loss
-        
-        return super().generic_step(batch, batch_idx, phase)
+#     def generic_step(self, batch, batch_idx, phase: str):
+#         return LFMAML.generic_step(self, batch, batch_idx, phase)
+    
+#     def on_before_optimizer_step(self, optimizer: Optimizer):
+#         # Average the accumulated gradients and optimize
+#         for p in self.maml.parameters():
+#             if p.grad is not None:
+#                 p.grad.data.mul_(1.0 / meta_batch_size)
+            
