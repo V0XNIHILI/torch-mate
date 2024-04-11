d7bb453b1982b686ad4ffd94783d638a9f1c9ce9
Douwe den Blanken <douwedb@gmail.com>
2024-04-11 21:49:51 +0200

    Add __all__ to init py for models

--- src/torch_mate/models/__init__.py
+++ src/torch_mate/models/__init__.py
@@ -1,3 +1,9 @@
 from .ClassificationLSTM import ClassificationLSTM
 from .MLP import MLP
 from .LeNet5BNMaxPool import LeNet5BNMaxPool
+
+__all__ = [
+    "ClassificationLSTM",
+    "MLP",
+    "LeNet5BNMaxPool"
+]
