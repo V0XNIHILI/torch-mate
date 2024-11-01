48a4895d86ff24be845a44ed0677dcebea164f84
Douwe den Blanken <douwedb@gmail.com>
2024-11-01 16:12:44 +0100

    Delete MLP

--- src/torch_mate/models/__init__.py
+++ src/torch_mate/models/__init__.py
@@ -1,9 +1,7 @@
 from .ClassificationLSTM import ClassificationLSTM
-from .MLP import MLP
 from .LeNet5BNMaxPool import LeNet5BNMaxPool
 
 __all__ = [
     "ClassificationLSTM",
-    "MLP",
     "LeNet5BNMaxPool"
 ]
