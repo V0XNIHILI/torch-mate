045b6021a77b8e873eb24936eb5938fd2b02f17c
Douwe den Blanken <douwedb@gmail.com>
2025-01-26 15:05:48 +0100

    Add better typing and error messages to get_modules()

--- src/torch_mate/utils/get_modules.py
+++ src/torch_mate/utils/get_modules.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Dict, List, Tuple, Union
 
 import torch.nn as nn
 
 from torch_mate.utils import get_class_and_init
 
 
-def get_modules(base_module, config: Dict):
+def get_modules(base_module, config: Union[Dict, List, Tuple]):
     if type(config) is tuple:
         raise ValueError("A tuple of modules is not supported. Please use a list or a dict of modules.")
 
@@ -13,10 +13,16 @@ def get_modules(base_module, config: Dict):
         return nn.ModuleList([get_class_and_init(base_module, config_entry) for config_entry in config])
     
     # "name" is a reserved key for the model name in case of a single model
-    if type(config) is dict and "name" not in config:
-        keys = config.keys()
-        values = config.values()
+    if type(config) is dict:
+        if "name" not in config:
+            if len(config) != 0:
+                keys = config.keys()
+                values = config.values()
 
-        return nn.ModuleDict({key: get_class_and_init(base_module, value) for key, value in zip(keys, values)})
+                return nn.ModuleDict({key: get_class_and_init(base_module, value) for key, value in zip(keys, values)})
+            else:
+                raise ValueError("Invalid configuration. Must have a 'name' key.")
 
-    return get_class_and_init(base_module, config)
+        return get_class_and_init(base_module, config)
+    
+    raise TypeError("Invalid configuration type. Must be a list or dict.")
