9f12f66355043a1f9d469cc079efbd50f7163537
Douwe den Blanken <douwedb@gmail.com>
2026-04-15 01:51:13 +0200

    Rename all transforms files into snake case

--- src/torch_mate/data/transforms/__init__.py
+++ src/torch_mate/data/transforms/__init__.py
@@ -1,16 +1,16 @@
-from torch_mate.data.transforms.AddNoise import AddNoise
-from torch_mate.data.transforms.Cartesian2PolarCoordinates import \
+from torch_mate.data.transforms.add_noise import AddNoise
+from torch_mate.data.transforms.cartesian2_polar_coordinates import \
     Cartesian2PolarCoordinates
-from torch_mate.data.transforms.DiscreteRandomCoordinateRotation import \
+from torch_mate.data.transforms.discrete_random_coordinate_rotation import \
     DiscreteRandomCoordinateRotation
-from torch_mate.data.transforms.DiscreteRandomRotation import \
+from torch_mate.data.transforms.discrete_random_rotation import \
     DiscreteRandomRotation
-from torch_mate.data.transforms.TimeShift import TimeShift
-from torch_mate.data.transforms.RandomResample import RandomResample
-from torch_mate.data.transforms.RandomVolume import RandomVolume
-from torch_mate.data.transforms.PermuteSequence import PermuteSequence
-from torch_mate.data.transforms.CenterCropOrPad import CenterCropOrPad
-from torch_mate.data.transforms.NormalizeSequence import NormalizeSequence
+from torch_mate.data.transforms.time_shift import TimeShift
+from torch_mate.data.transforms.random_resample import RandomResample
+from torch_mate.data.transforms.random_volume import RandomVolume
+from torch_mate.data.transforms.permute_sequence import PermuteSequence
+from torch_mate.data.transforms.center_crop_or_pad import CenterCropOrPad
+from torch_mate.data.transforms.normalize_sequence import NormalizeSequence
 
 __all__ = [
     "AddNoise",
