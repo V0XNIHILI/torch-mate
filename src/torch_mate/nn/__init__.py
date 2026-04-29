774009c93c4adb013e6bbb25afd1d6e1857a1787
Douwe den Blanken <douwedb@gmail.com>
2026-04-29 17:28:49 +0200

    Create transpose module

--- src/torch_mate/nn/__init__.py
+++ src/torch_mate/nn/__init__.py
@@ -1,9 +1,11 @@
 from torch_mate.nn.lambda_fn import Lambda
 from torch_mate.nn.one_hot import OneHot
 from torch_mate.nn.slice import Slice
+from torch_mate.nn.transpose import Transpose
 
 __all__ = [
     'Lambda',
     'OneHot',
     'Slice',
+    'Transpose',
 ]
---
+++ src/torch_mate/nn/transpose.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+class Transpose(nn.Module):
+    def __init__(self, dim0: int, dim1: int) -> None:
+        super().__init__()
+        self.dim0 = dim0
+        self.dim1 = dim1
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.transpose(self.dim0, self.dim1)
